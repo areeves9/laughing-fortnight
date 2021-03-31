6bd0e4a7e4146fe175c15d5da7d9c96a717c62b3
Andrew Reeves <areeves9@icloud.com>
2021-03-31 00:30:25 -0700

    Company model

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Company(models.Model):
+    chief_executive_officer = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    name = models.CharField(max_length=255, blank=False, null=False)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    url = models.URLField(blank=True, null=True)
+    is_licensed = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
