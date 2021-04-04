8d74cc1ac8091f04a509ed0f11b1b5f497b7a671
Andrew Reeves <areeves9@icloud.com>
2021-04-03 20:22:49 -0700

    createx experience and company tables in the db

--- companies/models.py
+++ companies/models.py
@@ -6,12 +6,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Company(models.Model):
-    chief_executive_officer = models.CharField(
-        max_length=255,
-        blank=True,
-        null=True
-    )
-    name = models.CharField(max_length=255, blank=False, null=False)
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
@@ -19,8 +13,10 @@ class Company(models.Model):
         blank=False,
         null=False,
     )
+    name = models.CharField(max_length=255, blank=False, null=False)
     us_phone = PhoneNumberField(blank=True, null=True)
     url = models.URLField(blank=True, null=True)
+
     is_licensed = models.BooleanField(default=False)
 
     class Meta:
@@ -35,18 +31,23 @@ class Experience(models.Model):
     PARTIME = 'part-time'
     CONTRACT = 'contract'
     EMPLOYMENT_TYPE = [
-        (FULLTIME, 'full-time employment'),
-        (PARTIME, 'part-time employment'),
+        (FULLTIME, 'full-time'),
+        (PARTIME, 'part-time'),
         (CONTRACT, 'contract'),
     ]
-    company = models.ForeignKey('Company', on_delete=models.PROTECT)
+    company = models.ForeignKey(
+        'Company',
+        on_delete=models.PROTECT,
+    )
     date_from = models.DateField()
     date_to = models.DateField(blank=True, null=True)
     description = models.TextField()
     employment_type = models.CharField(
         choices=EMPLOYMENT_TYPE,
+        default=FULLTIME,
+        max_length=150,
     )
-    title = models.CharField()
+    title = models.CharField(max_length=120)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
--- core/settings.py
+++ core/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
+    'companies',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
