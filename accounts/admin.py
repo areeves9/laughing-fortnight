f02b07efec7e33a892a2e70fa7403f42f5aa5069
Andrew Reeves <areeves9@icloud.com>
2021-04-20 13:04:28 -0700

    admin accounts fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,8 +4,25 @@ from accounts.models import SiteUser
 
 
 class SiteUserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'last_login',)
-    fields = ('headline', 'city', 'about', 'phone', 'skills', 'groups')
+    list_display = (
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff',
+        'last_login'
+    )
+    fields = (
+        'headline',
+        'city',
+        'about',
+        'phone',
+        'skills',
+        'groups',
+        'user_permissions',
+        'is_staff',
+        'is_superuser'
+    )
 
 
 admin.site.register(SiteUser, SiteUserAdmin)
